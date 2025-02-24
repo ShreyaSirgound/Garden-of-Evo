5a8485213c7b65add2f29ade2f08eaf2834dcdae
ShreyaSirgound <95923789+ShreyaSirgound@users.noreply.github.com>
2025-02-24 17:57:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext('2d')
 
+let myFont = new FontFace(
+    "Pixelify Sans",
+    "url(https://fonts.gstatic.com/s/pixelifysans/v1/CHylV-3HFUT7aC4iv1TxGDR9Jn0Eiw.woff2)"
+);
+
 const collisionsMap = []
 for (let i = 0; i < collisions.length; i += 80) {
     collisionsMap.push(collisions.slice(i, 80 + i))
@@ -21,6 +26,7 @@ class Boundary {
     }
 }
 
+const giftCoors = [[340, 150], [10, 200], [150, 420], [640, 440], [540, 330], [910, 240], [800, 30]]
 const boundaries = []
 
 collisionsMap.forEach((row, i) => {
@@ -43,9 +49,10 @@ const playerFront = new Image()
 const playerBack = new Image()
 const playerLeft = new Image()
 const playerRight = new Image()
+const giftImg = new Image()
 
 function loadImages() {
-    var imgToLoad = 5, imgLoaded = 0;
+    var imgToLoad = 6, imgLoaded = 0;
 
     var onImgLoad = function()
     {
@@ -69,10 +76,12 @@ function loadImages() {
     playerRight.src = 'assets\\sprite-assets\\walk-right.png'
     playerRight.onload = onImgLoad;
 
+    giftImg.src = 'assets\\giftbox.png'
+    giftImg.onload = onImgLoad;
 }
 
 class Sprite {
-    constructor({position, velocity, image, sprites = {}}) {
+    constructor({position, image, sprites = {}}) {
         this.position = position
         this.image = image
         this.width = 32
@@ -106,6 +115,30 @@ class Sprite {
                 }
             }
         }
+    }
+}
+
+class GiftSprite {
+    constructor({position}) {
+        this.position = position
+        this.width = 36
+        this.height = 45
+        this.clipy = 0
+        this.frames = 0
+    }
+
+    draw() {
+        ctx.drawImage(
+            giftImg,
+            this.frames * this.width,
+            0,
+            this.width,
+            this.height,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
     }
 }
 
@@ -147,16 +180,53 @@ function rectangularCollision({rect1, rect2}) {
     )
 }
 
+const gifts = []
+for(let i = 0; i < giftCoors.length; i++){
+    const giftbox = new GiftSprite({
+        position: {
+            x: giftCoors[i][0],
+            y: giftCoors[i][1]
+        }
+    })
+    gifts.push(giftbox)
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     ctx.drawImage(map, 0, 0)
     boundaries.forEach(boundary => {
         boundary.draw()
     })
+    gifts.forEach(gift => {
+        gift.draw()
+    })
     player.draw()
 
     let moving = true
     player.moving = false
+
+    for (let i = 0; i < giftCoors.length; i++) {
+        const gift = gifts[i]
+        if (rectangularCollision({
+            rect1: player,
+            rect2: {...gift,
+                position: {
+                    x: gift.position.x,
+                    y: gift.position.y
+                }
+            }})
+        ) {
+            myFont.load().then((font) => {
+                document.fonts.add(font);
+            
+                ctx.font = '15px Pixelify';
+                ctx.fillStyle = 'white';
+                ctx.fillText("Click to open!", gift.position.x - 30, gift.position.y);
+            });
+            break
+        }
+    }
+
     if (keys.ArrowUp.pressed) {
         player.moving = true
         player.image = player.sprites.back
